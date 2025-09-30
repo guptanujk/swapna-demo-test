d2f1de16f36e22e00d7d12a4ed980ac43662ac9a
guptanujk <guptanujk@gmail.com>
2025-09-30 17:23:57 +0530

    Update config.ts

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,5 +1,5 @@
 const environment = import.meta.env.VITE_ENVIRONMENT || 'dev';
-const baseURL = import.meta.env.VITE_BASEURL || 'http://localhost:3001';
+const baseURL = import.meta.env.VITE_BASEURL || 'http://backend:3001';
 export default {
   environment,
   baseURL,
